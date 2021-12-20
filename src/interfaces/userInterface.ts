14876d536e5f47f28942e82b21349226567f221b
Nivea <nivea@respondeai.com.br>
2021-12-20 11:39:04 -0300

    preparando para o review

--- src/interfaces/userInterface.ts
+++
@@ -1,8 +0,0 @@
-interface User {
-    id?: number;
-    name: string;
-    class: string;
-    token: string;
-}
-
-export default User;
--- src/server.ts
+++
@@ -1,7 +0,0 @@
-import './setup';
-import app from './app';
-
-app.listen(process.env.PORT, () => {
-    console.log(`Server is running on port ${process.env.PORT}!`);
-    console.log(`Application running in ${process.env.NODE_ENV} mode`);
-});
